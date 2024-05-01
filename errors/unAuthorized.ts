46d2d810b3d02cf63bf399a46bd4e4e3972cfbd6
princeajuzie7 <princeajuzie1@gmail.com>
2024-05-01 12:44:55 +0100

    docstring comment for db connection

--- errors/unAuthorized.ts
+++ errors/unAuthorized.ts
@@ -1,10 +1,27 @@
 import { UNAUTHORIZED } from "http-status";
 import CustomError from "./customError";
 
-export class UnAuthorized extends CustomError{
-    statusCode:number;
-    constructor(message:string){
-        super(message)
+/**
+ * Represents an unauthorized error.
+ *
+ * @extends {CustomError} - The base error class.
+ */
+export class UnAuthorized extends CustomError {
+    /**
+     * The HTTP status code for this error.
+     *
+     * @type {number}
+     */
+    statusCode: number;
+    /**
+     * Initializes an instance of the UnAuthorized class.
+     *
+     * @param {string} message - The error message.
+     */
+    constructor(message: string) {
+        // Calling super constructor of the base class
+        super(message);
+        // Setting the HTTP status code for this error
         this.statusCode = UNAUTHORIZED;
     }
 }
